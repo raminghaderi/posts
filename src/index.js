fafe52c42ac754c0f2144b163608823abe606bca
Ramin Ghaderi <raminghaderi77@gmail.com>
2020-06-01 15:41:44 +0200

    app setup #1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
